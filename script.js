7f869e3ebb148ec4272256e17a6c4cb9db212a77
Smart Pig by Felipe <felipediogo@live.com>
2022-03-29 15:43:18 -0300

    okok

--- script.js
+++ script.js
@@ -6,4 +6,6 @@ const min = data.getMinutes()
 const sec = data.getSeconds()
 
 hora.innerHTML = `Agora são: ${hour} horas, ${min} minutos e ${sec} segundos.`
-foto.src= "./lib/c-pedradagavea.svg"
+//foto.innerHTML = `<img src= "./lib/c-praiadoperigoso.svg">`
+img.src="./lib/c-praiadoperigoso.svg"
+
